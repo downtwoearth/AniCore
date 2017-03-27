d33bf1ef3e11ed2339a77e135457a622a34a8aa1
Kyle Brown <Kyle.Brown@ctt.co.uk>
2017-03-27 12:09:23 +0100

    Initial Commit

---
+++ AniCore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AniCore
+//
+//  Created by Kyle Brown on 27/03/2017.
+//  Copyright © 2017 Kyle Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
